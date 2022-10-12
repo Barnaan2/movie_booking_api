93f4dd6eafbf41326bd7bb5cbc9394d54b2ea0c8
Barnaan2 <barnaantekalign@gmail.com>
2022-10-11 17:57:05 -0700

    a BookingRequest model is added to the booking app

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import BookingRequest
+admin.site.register(BookingRequest)
 # Register your models here.
--- booking/models.py
+++ booking/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from cinema.models import MovieShow
 
 
-# # Create your models here.
-# class BookingRequest(models.Model):
-#     user_email = models.CharField(max_length=200)
-#     seat =models.ForeignKey(Seat)
+# Create your models here.
+class BookingRequest(models.Model):
+    movie_show = models.ForeignKey(MovieShow,on_delete=models.CASCADE)
+    user_email = models.CharField(max_length=200,null=True)
+    user_phone_number = models.CharField(max_length=200)
+    seat =models.CharField(max_length = 100)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.movie_show
+
