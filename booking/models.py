659523d4596d28eb772769ce1fa70e69d8d59fb1
Barnaan2 <barnaantekalign@gmail.com>
2022-12-07 09:54:36 -0800

    Aduience model and other logics are for seat reservation is added

--- booking/models.py
+++ booking/models.py
@@ -3,10 +3,10 @@ from cinema.models import MovieShow,MovieshowSeat
 from django.contrib.auth.models import User
 
 class Book(models.Model):
-    movie_show_seat = models.ForeignKey(MovieshowSeat,on_delete=models.CASCADE)
+    movie_show_seat=models.ManyToManyField(MovieshowSeat,null=True,blank=True) 
     user = models.ForeignKey(User,on_delete=models.SET_NULL)
     price = models.FloatField()
-    status = models.CharField(default="waiting",max_length=20)
+    status = models.CharField(default="waiting",max_length=20,null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
@@ -14,3 +14,9 @@ class Book(models.Model):
     def __str__(self):
         return self.movie_show_seat
  
+
+class Audience(models.Model): 
+    book = models.ForeignKey(Book,on_delete=models.CASCADE,null=True,blank=True)
+    movie_show_seat = models.ForeignKey(MovieshowSeat,on_delete=models.CASCADE,null=True,blank=True)
+    name=models.CharField(max_length=255, null=True)
+    contact=models.CharField(max_length=255, null=True)
