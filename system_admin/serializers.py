1ee2cbb01d9193db6695933aa96e3bca6042ba6a
Barnaan2 <barnaantekalign@gmail.com>
2022-12-18 14:16:53 -0800

    some routes are added and models are migrated

--- system_admin/serializers.py
+++ system_admin/serializers.py
@@ -19,10 +19,17 @@ class CrewSerializer(serializers.ModelSerializer):
       fields = '__all__'
    
 class MovieSerializer(serializers.ModelSerializer):
+   cast = CastSerializer(many=True)
+   crew = CrewSerializer(many=True)
+   # crew_data = serializers.SerializerMethodField()
+   
    class Meta:
       model = Movie
       fields = '__all__'
-
+   # def get_crew_data(self,obj):
+   #     crew = obj.objects.all()
+   #     datax = CrewSerializer(crew, many=True)
+   #     return datax.data
 class CinemaSerializer(serializers.ModelSerializer):
    class Meta:
       model = Cinema
